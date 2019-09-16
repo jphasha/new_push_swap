46416a9f8842adf3bdc49f840e7624610d560e48
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-09-16 15:58:46 +0200

    push_swap.h

---
+++ push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/16 15:31:20 by jphasha           #+#    #+#             */
+/*   Updated: 2019/09/16 15:57:14 by jphasha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+
+typedef struct		s_stack;
+{
+	int				data;
+	struct s_stack	*next
+}					t_stack;
+
+#endif
