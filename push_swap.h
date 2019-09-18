27c8e57c57b508306ca10b4ca13f23e9e6b293ef
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-09-18 13:44:49 +0200

    ft_stack_man2

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 15:31:20 by jphasha           #+#    #+#             */
-/*   Updated: 2019/09/17 18:45:56 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/09/18 12:56:35 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,13 @@ typedef struct		s_stack
 	int				data;
 	struct s_stack	*next;
 }					t_stack;
+int					ft_is_sorted(t_stack **head);
 void				ft_delbot_node(t_stack **head);
 void				ft_deltop_node(t_stack **head);
-void				ft_disp_list(t_stack *hd_a, t_stack *hd_b);
+void				ft_disp_list(t_stack **hd_a, t_stack **hd_b);
 void				ft_new_node(int data, t_stack **head);
 void				ft_push_top(int data, t_stack **head);
 void				ft_push_bottom(int data, t_stack **head);
+void				ft_srtd_stck(t_stack **hd_a, t_stack **hd_b);
 
 #endif
--- shared_srcs/ft_stack_man2.c
+++ shared_srcs/ft_stack_man2.c
@@ -6,19 +6,19 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 18:23:00 by jphasha           #+#    #+#             */
-/*   Updated: 2019/09/17 18:45:59 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/09/18 13:04:15 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void			ft_disp_list(t_stack *hd_a, t_stack *hd_b)
+void			ft_disp_list(t_stack **hd_a, t_stack **hd_b)
 {
 	t_stack		*disp_list_a;
 	t_stack		*disp_list_b;
 
-	disp_list_a = hd_a;
-	disp_list_b = hd_b;
+	disp_list_a = *hd_a;
+	disp_list_b = *hd_b;
 	if (hd_a != NULL)
 		ft_putendl("stack A:");
 	else
@@ -39,4 +39,26 @@ void			ft_disp_list(t_stack *hd_a, t_stack *hd_b)
 		ft_putchar('\n');
 		disp_list_b = disp_list_b->next;
 	}
+}
+
+void			ft_srtd_stck(t_stack **hd_a, t_stack **hd_b)
+{
+	if (ft_is_sorted(hd_a) && !*hd_b)
+		ft_putendl("OK");
+	else
+		ft_putendl("KO");
+}
+
+int				ft_is_sorted(t_stack **head)
+{
+	t_stack		*st_cp;
+
+	st_cp = *head;
+	while (st_cp->next != NULL)
+	{
+		if (st_cp->data > st_cp->next->data)
+			return (0);
+		st_cp = st_cp->next;
+	}
+	return (1);
 }
