1f09119a8d554f84df7ed62def0d08d37c1318cb
Johannes PHASHA <jphasha@c4r5s11.wethinkcode.co.za>
2019-09-23 15:43:02 +0200

    update

--- checker_srcs/checker.c
+++ checker_srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 16:01:03 by jphasha           #+#    #+#             */
-/*   Updated: 2019/09/23 15:02:02 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/09/23 15:42:24 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,21 @@
 
 int		main(int ac, char *av[])
 {
-	int	i;
+	if (ac == 1)
+		return (0);
+	int		i;
+	t_stack	*stack_A;
+	t_stack	*stack_B;
+	char	**args;
 
+	stack_A = NULL;
+	stack_B = NULL;
 	i = 0;
-	if (ac > 1)
+	args = ft_get_args(av, &stack_A, ac);
+	if (ft_error_handle(args))
 	{
-		while (av[i])
-		{
-			ft_putendl(av[i]);
-			i++;
-		}
 	}
+	ft_loop_memdel((void **)args);
+	ft_del_stack(&stack_A, &stack_B);
 	return (0);
 }
