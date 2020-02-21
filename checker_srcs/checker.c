97651bd1db6f12a0ce9bd58dbaf0c51f9522693f
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2020-02-21 10:51:55 +0200

    a function that executes valid instructions and print Error in the standard error if the instruction is invalid

--- checker_srcs/checker.c
+++ checker_srcs/checker.c
@@ -12,7 +12,38 @@
 
 #include "../push_swap.h"
 
-int		main(int ac, char *av[])
+static void	ft_exec_instrs(char *instr, t_stack **stack_A, t_stack **stack_B)
+{
+	if (!ft_strcmp(instr, "sa"))
+		ft_sa(stack_A);
+	else if (!ft_strcmp(instr, "sb"))
+		ft_sb(stack_B);
+	else if (!ft_strcmp(instr, "ss"))
+		ft_ss(stack_A, stack_B);
+	else if (!ft_strcmp(instr, "pa"))
+		ft_pa(stack_A, stack_B);
+	else if (!ft_strcmp(instr, "pb"))
+		ft_pb(stack_A, stack_B);
+	else if (!ft_strcmp(instr, "ra"))
+		ft_ra(stack_A);
+	else if (!ft_strcmp(instr, "rb"))
+		ft_rb(stack_B);
+	else if (!ft_strcmp(instr, "rr"))
+		ft_rr(stack_A, stack_B);
+	else if (!ft_strcmp(instr, "rra"))
+		ft_rra(stack_A);
+	else if (!ft_strcmp(instr, "rrb"))
+		ft_rrb(stack_B);
+	else if (!ft_strcmp(instr, "rrr"))
+		ft_rrr(stack_A, stack_B);
+	else
+	{
+		ft_putendl_fd("Error", 2);
+		exit (0);
+	}
+}
+
+int			main(int ac, char *av[])
 {
 	if (ac == 1)
 		return (0);
@@ -33,9 +64,8 @@ int		main(int ac, char *av[])
 			{
 				if (ft_valid_instr(instr))
 				{
-					/* lt's sort according to the instructions
-					but for now let us fix the 'ft_valid_instr first' */
-					ft_putendl("yay! you have a working instruction");
+					ft_exec_instrs(instr, &stack_A, &stack_B);
+					ft_disp_list(&stack_A, &stack_B);
 				}
 
 				else
