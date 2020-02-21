c171d5b63840712fbdde2e3afb4eb1639ed4732e
jphasha <50695096+jphasha@users.noreply.github.com>
2020-02-21 07:53:31 +0200

    Update ft_arg_man.c

--- shared_srcs/ft_arg_man.c
+++ shared_srcs/ft_arg_man.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-#include <stdio.h>
 
 static void	ft_sto_args(char **ah, char **av)
 {
